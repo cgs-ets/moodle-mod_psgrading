de193bac3f4d9824dbf9844e3babde0041a212e6
Michael Vangelovski <michael.vangelovski@hotmail.com>
2024-04-10 08:37:05 +1000

    Add quickmark

--- mark.php
+++ mark.php
@@ -266,7 +266,7 @@ echo $OUTPUT->header();
 
 if ($qm == '1') {
     echo "<style> .psgrading-header-right { display: none !important; } d</style>";
-    echo '<div class="alert alert-warning">Quick mark is in beta testing. If you have any feedback please contact the Service Desk.</div>';
+    echo '<div class="alert alert-warning">Alt mark is an experiemental feature in beta testing. If you would like to pass on any feedback please direct it to <a href="mailto:michael.vangelovski@cgs.act.edu.au">MV</a>.</div>';
     echo "<a target='_top' class='btn btn-primary mb-3' href='{$listurl}' ><i class='fa fa-arrow-left'></i> Back to grades table</a>";
 }
 
